9db9c956e1a0e27e7a5793a5c5d0ddd195cdc66c
catolicodrg <dgc5166@students.uc-bcf.edu.ph>
2025-04-08 21:06:21 +0800

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,32 +1,44 @@
 import java.util.List;
 
-class User {
-    protected int id;
-    protected String username;
-    protected String password;
-    protected List<Recipe> recipes;
-    
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private List<Recipe> recipes;
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
     public boolean login(String username, String password) {
         return this.username.equals(username) && this.password.equals(password);
     }
-    
-    public void logout() {}
-    
+
+    public void logout() {
+        System.out.println("User logged out.");
+    }
+
     public boolean register(String username, String password) {
         this.username = username;
         this.password = password;
         return true;
     }
-    
+
     public void createRecipe(Recipe recipe) {
         recipes.add(recipe);
     }
-    
-    public void editRecipe(int recipeId, Recipe recipe) {}
-    
-    public void deleteRecipe(int recipeId) {}
-    
+
+    public void editRecipe(int recipeId, Recipe recipe) {
+        recipes.set(recipeId, recipe);
+    }
+
+    public void deleteRecipe(int recipeId) {
+        recipes.remove(recipeId);
+    }
+
     public Recipe viewRecipe(int recipeId) {
+        return recipes.get(recipeId);
     }
 }
-
