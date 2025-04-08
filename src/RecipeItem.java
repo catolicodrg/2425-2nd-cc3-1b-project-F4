4a5b7ce6041d467738217c3e48d55fbb8ad0f6ce
catolicodrg <dgc5166@students.uc-bcf.edu.ph>
2025-04-08 21:13:40 +0800

    Update RecipeItem.java

--- src/RecipeItem.java
+++ src/RecipeItem.java
@@ -1,9 +1,27 @@
-class RecipeItem {
+public abstract class RecipeItem {
     protected int id;
     protected String name;
-    
+
+    public RecipeItem(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
-}
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}    
