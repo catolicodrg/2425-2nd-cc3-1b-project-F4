ab7a8ff17e830304c7d37a85e9faf8371635a5dd
catolicodrg <dgc5166@students.uc-bcf.edu.ph>
2025-04-08 21:14:04 +0800

    Update RecipeManager.java

--- src/RecipeManager.java
+++ src/RecipeManager.java
@@ -1,20 +1,29 @@
 import java.util.List;
 
-class RecipeManager {
+public class RecipeManager {
     private List<Recipe> recipes;
-    
+
     public void addRecipe(Recipe recipe) {
         recipes.add(recipe);
     }
-    
-    public void deleteRecipe(int recipeId) {}
-    
-    public void updateRecipe(int recipeId, Recipe recipe) {}
-    
-    public Recipe getRecipe(int recipeId) {
-        return recipes.stream().filter(r -> r.id == recipeId).findFirst().orElse(null);
+
+    public void deleteRecipe(int recipeId) {
+        recipes.removeIf(recipe -> recipe.getId() == recipeId);
     }
-    
+
+    public void updateRecipe(int recipeId, Recipe recipe) {
+        for (int i = 0; i < recipes.size(); i++) {
+            if (recipes.get(i).getId() == recipeId) {
+                recipes.set(i, recipe);
+                break;
+            }
+        }
+    }
+
+    public Recipe getRecipe(int recipeId) {
+        return recipes.stream().filter(recipe -> recipe.getId() == recipeId).findFirst().orElse(null);
+    }
+
     public List<Recipe> listRecipes() {
         return recipes;
     }
