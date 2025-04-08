8c7eb98f592d5d364e05a7a6f567aba87bbbd357
catolicodrg <dgc5166@students.uc-bcf.edu.ph>
2025-04-08 21:12:42 +0800

    Update Ingredient.java

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,7 +1,12 @@
-class Ingredient extends RecipeItem {
+public class Ingredient extends RecipeItem {
     private String quantity;
-    
+
+    public Ingredient(int id, String name, String quantity) {
+        super(id, name);
+        this.quantity = quantity;
+    }
+
     public String getDetails() {
-        return name + " - " + quantity;
+        return name + " (" + quantity + ")";
     }
 }
