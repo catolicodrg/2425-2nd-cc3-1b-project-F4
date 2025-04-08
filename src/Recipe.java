c95753474713e4e16b8c0e26a9cd0c880157eee4
catolicodrg <dgc5166@students.uc-bcf.edu.ph>
2025-04-08 21:13:13 +0800

    Update Recipe.java

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,36 +1,49 @@
 import java.util.List;
 
-class Recipe extends RecipeItem {
+public class Recipe extends RecipeItem {
     private String description;
     private List<Ingredient> ingredients;
     private List<String> steps;
     private Category category;
     private List<Integer> ratings;
-    
+
+    public Recipe(int id, String name, String description, List<Ingredient> ingredients, List<String> steps, Category category) {
+        super(id, name);
+        this.description = description;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.category = category;
+    }
+
     public void addIngredient(Ingredient ingredient) {
         ingredients.add(ingredient);
     }
-    
+
     public void removeIngredient(Ingredient ingredient) {
         ingredients.remove(ingredient);
     }
-    
+
     public void editRecipe(String name, String description, List<String> steps) {
         this.name = name;
         this.description = description;
         this.steps = steps;
     }
-    
+
     public void displayRecipe() {
         System.out.println("Recipe: " + name);
+        System.out.println("Description: " + description);
+        System.out.println("Ingredients: " + ingredients);
+        System.out.println("Steps: " + steps);
+        System.out.println("Category: " + category.getName());
     }
-    
+
     public void rateRecipe(int rating) {
         ratings.add(rating);
     }
-    
+
     public float getAverageRating() {
-        return (float) ratings.stream().mapToInt(i -> i).average().orElse(0);
+        if (ratings.isEmpty()) return 0;
+        int sum = ratings.stream().mapToInt(Integer::intValue).sum();
+        return (float) sum / ratings.size();
     }
-    
 }
